a38a8e59dcd1ac7eb7484af47b1a4bab36cedeb6
Mohit Bodkhe <mohitbodkhe2479@gmail.com>
2024-07-30 10:49:55 +0530

    add blur radius

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,137 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import React from "react";
+import { Button, Typography, Container } from "@mui/material";
+import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
+import { styled } from "@mui/system";
+import styles from "../styles/NotificationIcon.module.css";
+
+const Card = styled("div")({
+  backgroundImage: "linear-gradient(to bottom, #573cbb, #1c142d, #0e0e0d)",
+  backgroundColor: "#1A1A1A",
+  borderRadius: "10px",
+  textAlign: "center",
+  padding: "20px",
+  width: "100wh",
+  height: "100vh",
+  maxWidth: "400px",
+  border: "1px solid #6200EA",
+});
+
+const Title = styled(Typography)({
+  color: "#fff",
+  marginTop: "10px",
+  fontSize: "18px",
+});
+
+const Subtitle = styled(Typography)({
+  fontWeight: "bold",
+  color: "#fff",
+  marginTop: "0.2rem",
+  fontSize: "14px",
+});
+
+const NotificationButton = styled(Button)({
+  backgroundColor: "#3b2160",
+  border: "0.25rem solid #7f2a9c",
+  borderRadius: "0.55rem",
+  color: "#fff",
+  width: "18rem",
+  marginTop: "3rem",
+  "&:hover": {
+    backgroundColor: "#3b2160",
+  },
+});
+
+const Home: React.FC = () => {
+  const sendNotification = () => {
+    if (Notification.permission === "granted") {
+      new Notification("Hug × Hug", {
+        body: "Lorem ipsum dolor sit amet.",
+      });
+    } else if (Notification.permission !== "denied") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          new Notification("Hug × Hug", {
+            body: "Lorem ipsum dolor sit amet.",
+          });
+        }
+      });
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <Card>
+      <Title variant="h6" style={{ margin: "1rem 5rem", fontSize: "20px" }}>
+        Hola!
+      </Title>
+      {/* <div
+        style={{
+          height: "11rem",
+          width: "11rem",
+          borderRadius: "22px",
+          position: "absolute",
+          top: "10rem",
+          left: "7rem",
+        }}
+      >
+        <div className={styles.iconContainer}>
+          <div className={styles.borderCircle}></div>
+          <div className={styles.icon}>
+            <NotificationsActiveIcon
+              style={{ fontSize: "80px", color: "#fff" }}
             />
-          </a>
+          </div>
+        </div>
+      </div> */}
+      <div
+        style={{
+          height: "11rem",
+          width: "11rem",
+          borderRadius: "22px",
+          position: "absolute",
+          top: "14rem",
+          left: "7rem",
+        }}
+      >
+        <div className={styles.iconContainer}>
+          <div className={styles.outerBlur}></div>
+          <div className={styles.middleLight}></div>
+          <div className={styles.innerDark}></div>
+          <div className={styles.icon}>
+            <NotificationsActiveIcon
+              style={{ fontSize: "80px", color: "#fff" }}
+            />
+          </div>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <Subtitle
+        variant="body1"
+        style={{
+          fontSize: "1.1rem",
+          marginTop: "33rem",
+          fontWeight: "bold",
+          display: "block",
+        }}
+      >
+        Lorem Ipsum...
+      </Subtitle>
+      <Subtitle
+        variant="body2"
+        style={{
+          color: "#d5d5d5",
+        }}
+      >
+        Lorem ipsum dolor sit amet.
+      </Subtitle>
+      <NotificationButton
+        variant="contained"
+        onClick={() => sendNotification()}
+      >
+        Send Notification
+      </NotificationButton>
+    </Card>
   );
-}
+};
+
+export default Home;
